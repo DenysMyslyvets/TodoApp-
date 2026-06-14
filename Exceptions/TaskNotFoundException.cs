9a379f917795e4cfda779bccff662b1d4232fe12
Denys Myslyvets <denys.myslyvets@academicschool.cz>
2026-06-14 15:29:57 +0200

    Doplnění komentářů do TaskService

--- Exceptions/TaskNotFoundException.cs
+++ Exceptions/TaskNotFoundException.cs
@@ -1,12 +1,14 @@
 namespace TodoApp.Exceptions;
 
+// Vlastní výjimka vyvolaná v případě, že úkol s daným ID neexistuje
 public class TaskNotFoundException : Exception
 {
-     public int TaskId { get; }
+    // ID nenalezeného úkolu
+    public int TaskId { get; }
 
     public TaskNotFoundException(int taskId)
         : base("Úkol s ID " + taskId + " nebyl nalezen.")
     {
-          TaskId = taskId;
+        TaskId = taskId;
     }
 }
